b1b3282eea4940d859ce2f2eff824e09c42a3ccb
Dan Olson <danole@comcast.net>
2016-03-29 22:38:20 -0700

    Simple queue demo
    Demo two tasks communicating with a queue. Builds off the morse code
    demo.

--- Demo/main.c
+++ Demo/main.c
@@ -1,8 +1,19 @@
 #include <FreeRTOS.h>
+#include <queue.h>
 #include <task.h>
 
 #include "Drivers/interrupts.h"
 #include "Drivers/gpio.h"
+
+enum DemoType {
+  MorsecodeDemo,
+  SosDemo,
+  BlinkyDemo
+};
+
+// Pick demo type to run
+enum DemoType demoType = MorsecodeDemo;
+
 
 #define UNUSED(v) (void)(v)
 
@@ -19,6 +30,48 @@ void dash(void) {
   SetGpio(16, 1);
   vTaskDelay(200);
 }
+
+xQueueHandle morsecodeQueue = NULL;
+
+void senderTask(void *pParam) {
+  UNUSED(pParam);
+  char s = 's';
+  char o = 'o';
+  for (;;) {
+    xQueueSend(morsecodeQueue, &s, 0);
+    xQueueSend(morsecodeQueue, &o, 0);
+    xQueueSend(morsecodeQueue, &s, 0);
+    vTaskDelay(20000);
+  }
+}
+
+void morsecodeTask(void *pParam) {
+  UNUSED(pParam);
+
+  for (;;) {
+    char letter = ' ';
+    if (xQueueReceive(morsecodeQueue, &letter, 1000)) {
+      if (letter == 's') {
+	dot();
+	dot();
+	dot();
+      }
+      else if (letter == 'o') {
+	dash();
+	dash();
+	dash();
+      }
+      else {
+	// FIXME: add all the other letters and numbers
+	dash();
+      }
+    }
+    else {
+      dot();
+    }
+  }
+}
+
 
 void sos(void *pParam) {
   UNUSED(pParam);
@@ -76,9 +129,20 @@ int main(void) {
 
 	SetGpioFunction(16, 1);			// RDY led
 
-	xTaskCreate(sos, (signed char*)"SOS", 128, NULL, 0, NULL);
-	//xTaskCreate(task1, (signed char*)"LED_0", 128, NULL, 0, NULL);
-	//xTaskCreate(task2, (signed char*)"LED_1", 128, NULL, 0, NULL);
+	switch (demoType) {
+	case MorsecodeDemo:
+	  morsecodeQueue = xQueueCreate(10, sizeof(char));
+	  xTaskCreate(senderTask, (signed char*)"Sender", 128, NULL, 0, NULL);
+	  xTaskCreate(morsecodeTask, (signed char*)"MorseCode", 128, NULL, 0, NULL);
+	  break;
+	case SosDemo:
+	  xTaskCreate(sos, (signed char*)"SOS", 128, NULL, 0, NULL);
+	  break;
+	case BlinkyDemo:
+	  xTaskCreate(task1, (signed char*)"LED_0", 128, NULL, 0, NULL);
+	  xTaskCreate(task2, (signed char*)"LED_1", 128, NULL, 0, NULL);
+	  break;
+	}
 
 	vTaskStartScheduler();
 
