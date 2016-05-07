31a1068fddb8cbaef7e29607dcc6af32d84b802d
Dan Olson <danole@comcast.net>
2016-05-07 15:42:12 -0700

    Startup asm obeys RPI setting
    Changed startup.s to .S extension so will be run through C preprocessor.
    The assembly code follows RPI setting so can work for Pi 1, 2, or 3

--- .dbuild/asm-objects.mk
+++ .dbuild/asm-objects.mk
@@ -1,7 +1,15 @@
 
+# extension .s is just assembled by as assembler
+# extension .S is tradtionally assembled via cc and can see C macros
+
 $(BUILD_DIR)%.o: $(BASE)%.s
 	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(notdir $@)
 	@mkdir -p $(dir $@)
 	$(Q)$(AS) $(ASFLAGS) $< -o $@
+
+$(BUILD_DIR)%.o: $(BASE)%.S
+	$(Q)$(PRETTY) --dbuild "AS" $(MODULE_NAME) $(notdir $@)
+	@mkdir -p $(dir $@)
+	$(Q)$(CC) $(CFLAGS) $< -o $@
 
 
--- dbuild.config.mk
+++ dbuild.config.mk
@@ -1,6 +1,9 @@
 
-#CFLAGS += -march=armv6z -g -Wall -Wextra -DRPI=3
-CFLAGS += -march=armv7-a -g -Wall -Wextra -DRPI=3
+# Select Raspberry PI model (1, 2, or 3)
+#CFLAGS += -march=armv6z -g -Wall -Wextra -DRPI=1
+#CFLAGS += -march=armv7-a -g -Wall -Wextra -DRPI=2
+CFLAGS += -march=armv8-a -g -Wall -Wextra -DRPI=3
+
 CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
 CFLAGS += -I $(BASE)FreeRTOS/Source/include/
 CFLAGS += -I $(BASE)Demo/Drivers/
